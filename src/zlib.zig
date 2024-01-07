a6e4c26f021ce63f3e55b20281048dec3dd08c12
NyuB <brice.decaestecker@gmx.fr>
2024-01-07 07:19:58 +0100

    Added constant tables

--- src/zlib.zig
+++ src/zlib.zig
@@ -54,6 +54,30 @@ const CompressionMethodAndFlags = packed struct(u16) {
     }
 };
 
+const LengthCode = packed struct(u16) {
+    extra_bits: u3,
+    min_value: u13,
+};
+
+const LiteralLengthCodeTag = enum(u2) {
+    Literal,
+    Length,
+    EndOfBlock,
+    Unused,
+};
+
+const LiteralLengthCode = union(LiteralLengthCodeTag) {
+    Literal: u8,
+    Length: LengthCode,
+    EndOfBlock: void,
+    Unused: void,
+};
+
+const DistanceCode = struct {
+    extra_bits: u4,
+    min_value: u16,
+};
+
 const TestReaderWriter = struct {
     input: []const u8,
     output: []u8,
@@ -123,6 +147,163 @@ const TestReaderWriter = struct {
     }
 };
 
+// Fixed tables
+
+const LITERAL_LENGTH_CODE_TABLE: [288]LiteralLengthCode = fill: {
+    var table: [288]LiteralLengthCode = undefined;
+    for (0..256) |c| {
+        const char: u8 = @truncate(c);
+        table[c] = .{ .Literal = char };
+    }
+    table[256] = .{ .EndOfBlock = {} };
+
+    //         Extra               Extra               Extra
+    // Code Bits Length(s) Code Bits Lengths   Code Bits Length(s)
+    // ---- ---- ------     ---- ---- -------   ---- ---- -------
+    //     257   0     3       267   1   15,16     277   4   67-82
+    //     258   0     4       268   1   17,18     278   4   83-98
+    //     259   0     5       269   2   19-22     279   4   99-114
+    //     260   0     6       270   2   23-26     280   4  115-130
+    //     261   0     7       271   2   27-30     281   5  131-162
+    //     262   0     8       272   2   31-34     282   5  163-194
+    //     263   0     9       273   3   35-42     283   5  195-226
+    //     264   0    10       274   3   43-50     284   5  227-257
+    //     265   1  11,12      275   3   51-58     285   0    258
+    //     266   1  13,14      276   3   59-66
+
+    table[257] = .{ .Length = LengthCode{ .extra_bits = 0, .min_value = 3 } };
+    table[258] = .{ .Length = LengthCode{ .extra_bits = 0, .min_value = 4 } };
+    table[259] = .{ .Length = LengthCode{ .extra_bits = 0, .min_value = 5 } };
+    table[260] = .{ .Length = LengthCode{ .extra_bits = 0, .min_value = 6 } };
+    table[261] = .{ .Length = LengthCode{ .extra_bits = 0, .min_value = 7 } };
+    table[262] = .{ .Length = LengthCode{ .extra_bits = 0, .min_value = 8 } };
+    table[263] = .{ .Length = LengthCode{ .extra_bits = 0, .min_value = 9 } };
+    table[264] = .{ .Length = LengthCode{ .extra_bits = 0, .min_value = 10 } };
+
+    table[265] = .{ .Length = LengthCode{ .extra_bits = 1, .min_value = 11 } };
+    table[266] = .{ .Length = LengthCode{ .extra_bits = 1, .min_value = 13 } };
+    table[267] = .{ .Length = LengthCode{ .extra_bits = 1, .min_value = 15 } };
+    table[268] = .{ .Length = LengthCode{ .extra_bits = 1, .min_value = 17 } };
+
+    table[269] = .{ .Length = LengthCode{ .extra_bits = 2, .min_value = 19 } };
+    table[270] = .{ .Length = LengthCode{ .extra_bits = 2, .min_value = 23 } };
+    table[271] = .{ .Length = LengthCode{ .extra_bits = 2, .min_value = 27 } };
+    table[272] = .{ .Length = LengthCode{ .extra_bits = 2, .min_value = 31 } };
+
+    table[273] = .{ .Length = LengthCode{ .extra_bits = 3, .min_value = 35 } };
+    table[274] = .{ .Length = LengthCode{ .extra_bits = 3, .min_value = 43 } };
+    table[275] = .{ .Length = LengthCode{ .extra_bits = 3, .min_value = 51 } };
+    table[276] = .{ .Length = LengthCode{ .extra_bits = 3, .min_value = 59 } };
+
+    table[277] = .{ .Length = LengthCode{ .extra_bits = 4, .min_value = 67 } };
+    table[278] = .{ .Length = LengthCode{ .extra_bits = 4, .min_value = 83 } };
+    table[279] = .{ .Length = LengthCode{ .extra_bits = 4, .min_value = 99 } };
+    table[280] = .{ .Length = LengthCode{ .extra_bits = 4, .min_value = 115 } };
+
+    table[281] = .{ .Length = LengthCode{ .extra_bits = 5, .min_value = 131 } };
+    table[282] = .{ .Length = LengthCode{ .extra_bits = 5, .min_value = 163 } };
+    table[283] = .{ .Length = LengthCode{ .extra_bits = 5, .min_value = 195 } };
+    table[284] = .{ .Length = LengthCode{ .extra_bits = 5, .min_value = 227 } };
+    table[285] = .{ .Length = LengthCode{ .extra_bits = 0, .min_value = 258 } };
+
+    table[286] = .{ .Unused = {} };
+    table[287] = .{ .Unused = {} };
+
+    break :fill table;
+};
+
+const LITERAL_LENGTH_SYMBOLS: [288]u9 = fill: {
+    var symbols: [288]u9 = undefined;
+    for (0..288) |i| {
+        const symbol: u9 = @truncate(i);
+        symbols[i] = symbol;
+    }
+    break :fill symbols;
+};
+
+const FIXED_LITERAL_LENGTH_LENGTHS: [288]usize = fill: {
+    // Lit Value    Bits
+    // ---------    ----
+    //   0 - 143     8
+    // 144 - 255     9
+    // 256 - 279     7
+    // 280 - 287     8
+    var lengths: [288]usize = undefined;
+    fillPortion(0, 143, &lengths, 8);
+    fillPortion(144, 255, &lengths, 9);
+    fillPortion(256, 279, &lengths, 7);
+    fillPortion(280, 287, &lengths, 8);
+    break :fill lengths;
+};
+
+fn fillPortion(startInclusive: usize, endInclusive: usize, arr: []usize, value: usize) void {
+    for (startInclusive..endInclusive + 1) |i| {
+        arr[i] = value;
+    }
+}
+
+//         Extra           Extra               Extra
+// Code Bits Dist  Code Bits   Dist     Code Bits Distance
+// ---- ---- ----  ---- ----  ------    ---- ---- --------
+// 0   0    1     10   4     33-48    20    9   1025-1536
+// 1   0    2     11   4     49-64    21    9   1537-2048
+// 2   0    3     12   5     65-96    22   10   2049-3072
+// 3   0    4     13   5     97-128   23   10   3073-4096
+// 4   1   5,6    14   6    129-192   24   11   4097-6144
+// 5   1   7,8    15   6    193-256   25   11   6145-8192
+// 6   2   9-12   16   7    257-384   26   12  8193-12288
+// 7   2  13-16   17   7    385-512   27   12 12289-16384
+// 8   3  17-24   18   8    513-768   28   13 16385-24576
+// 9   3  25-32   19   8   769-1024   29   13 24577-32768
+const DISTANCE_TABLE = [32]DistanceCode{
+    DistanceCode{ .extra_bits = 0, .min_value = 1 },
+    DistanceCode{ .extra_bits = 0, .min_value = 2 },
+    DistanceCode{ .extra_bits = 0, .min_value = 3 },
+    DistanceCode{ .extra_bits = 0, .min_value = 4 },
+
+    DistanceCode{ .extra_bits = 1, .min_value = 5 },
+    DistanceCode{ .extra_bits = 1, .min_value = 7 },
+
+    DistanceCode{ .extra_bits = 2, .min_value = 9 },
+    DistanceCode{ .extra_bits = 2, .min_value = 13 },
+
+    DistanceCode{ .extra_bits = 3, .min_value = 17 },
+    DistanceCode{ .extra_bits = 3, .min_value = 25 },
+
+    DistanceCode{ .extra_bits = 4, .min_value = 33 },
+    DistanceCode{ .extra_bits = 4, .min_value = 49 },
+
+    DistanceCode{ .extra_bits = 5, .min_value = 65 },
+    DistanceCode{ .extra_bits = 5, .min_value = 97 },
+
+    DistanceCode{ .extra_bits = 6, .min_value = 129 },
+    DistanceCode{ .extra_bits = 6, .min_value = 193 },
+
+    DistanceCode{ .extra_bits = 7, .min_value = 257 },
+    DistanceCode{ .extra_bits = 7, .min_value = 385 },
+
+    DistanceCode{ .extra_bits = 8, .min_value = 513 },
+    DistanceCode{ .extra_bits = 8, .min_value = 769 },
+
+    DistanceCode{ .extra_bits = 9, .min_value = 1025 },
+    DistanceCode{ .extra_bits = 9, .min_value = 1537 },
+
+    DistanceCode{ .extra_bits = 10, .min_value = 2049 },
+    DistanceCode{ .extra_bits = 10, .min_value = 3073 },
+
+    DistanceCode{ .extra_bits = 11, .min_value = 4097 },
+    DistanceCode{ .extra_bits = 11, .min_value = 6145 },
+
+    DistanceCode{ .extra_bits = 12, .min_value = 8193 },
+    DistanceCode{ .extra_bits = 12, .min_value = 12289 },
+
+    DistanceCode{ .extra_bits = 13, .min_value = 16385 },
+    DistanceCode{ .extra_bits = 13, .min_value = 24577 },
+
+    DistanceCode{ .extra_bits = 0, .min_value = 0 },
+    DistanceCode{ .extra_bits = 0, .min_value = 0 },
+};
+
 // Tests
 
 const expect = std.testing.expect;
@@ -134,4 +315,34 @@ test "Decode" {
     const output = &[_]u8{};
     var testWR = TestReaderWriter{ .input = content, .output = output };
     try decode(&testWR, {});
+}
+
+test "Tables consistency" {
+    for (0..256) |c| {
+        const char: u8 = @truncate(c);
+        try expect(LITERAL_LENGTH_CODE_TABLE[c].Literal == char);
+    }
+
+    try expectEqualDeep(LiteralLengthCode{ .EndOfBlock = {} }, LITERAL_LENGTH_CODE_TABLE[256]);
+
+    for (257..284) |i| {
+        const len = LITERAL_LENGTH_CODE_TABLE[i].Length;
+        const next_len = LITERAL_LENGTH_CODE_TABLE[i + 1].Length;
+        try expect(next_len.min_value == len.min_value + utils.powerOfTwo(len.extra_bits));
+    }
+
+    try expectEqualDeep(LiteralLengthCode{ .Length = LengthCode{ .extra_bits = 0, .min_value = 258 } }, LITERAL_LENGTH_CODE_TABLE[285]);
+    try expectEqualDeep(LiteralLengthCode{ .Unused = {} }, LITERAL_LENGTH_CODE_TABLE[286]);
+    try expectEqualDeep(LiteralLengthCode{ .Unused = {} }, LITERAL_LENGTH_CODE_TABLE[287]);
+
+    for (0..29) |i| {
+        const dist = DISTANCE_TABLE[i];
+        const next_dist = DISTANCE_TABLE[i + 1];
+        try expect(next_dist.min_value == dist.min_value + utils.powerOfTwo(dist.extra_bits));
+    }
+}
+
+test "Memory layouts" {
+    try expect(@sizeOf(LiteralLengthCode) == 4); // max is u16 + enum tag
+    try expect(@sizeOf(DistanceCode) == 4); // max is u16 + extra_bits field
 }
