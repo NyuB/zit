a83cc1d4a07d4d5927c8606febcf42dda2f00ddc
NyuB <brice.decaestecker@gmx.fr>
2024-01-21 06:45:09 +0100

    Improve argparse with flag args and comptime checks

--- src/argparse.zig
+++ src/argparse.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const String = []const u8;
 
 pub fn argParse(args: []const String, comptime Spec: type) ParseError!Spec {
+    comptime validateSpec(Spec);
     const info = @typeInfo(Spec);
     var result: Spec = undefined;
     inline for (info.Struct.fields) |f| {
@@ -12,6 +13,48 @@ pub fn argParse(args: []const String, comptime Spec: type) ParseError!Spec {
         }
     }
     return result;
+}
+
+fn validateSpec(comptime Spec: type) void {
+    const info = @typeInfo(Spec);
+    inline for (info.Struct.fields) |f| {
+        if (@hasDecl(f.type, "parse")) {
+            validateParseFn(f);
+        } else if (@hasDecl(f.type, "set")) {
+            validateSetFn(f);
+        } else {
+            const msg = std.fmt.comptimePrint("Option {s} does not implement set() or parse()", .{f.name});
+            @compileError(msg);
+        }
+    }
+}
+
+fn validateParseFn(comptime f: std.builtin.Type.StructField) void {
+    const fnSpec = @typeInfo(@TypeOf(f.type.parse)).Fn;
+    const fnParams = fnSpec.params;
+    const expectedReturnType = ParseError!f.type;
+    if (fnParams.len != 1 or fnParams[0].type.? != String or fnSpec.return_type.? != expectedReturnType) {
+        var fnParamTypes: [fnParams.len]type = undefined;
+        inline for (fnParams, 0..) |p, i| {
+            fnParamTypes[i] = p.type.?;
+        }
+        const msg = std.fmt.comptimePrint("Option {s} does not implement parse({{ {any} }}){any}, found parse({any}){any}", .{ f.name, String, expectedReturnType, fnParamTypes, fnSpec.return_type });
+        @compileError(msg);
+    }
+}
+
+fn validateSetFn(comptime f: std.builtin.Type.StructField) void {
+    const fnSpec = @typeInfo(@TypeOf(f.type.set)).Fn;
+    const fnParams = fnSpec.params;
+    const expectedReturnType = ParseError!f.type;
+    if (fnParams.len != 0 or fnSpec.return_type.? != expectedReturnType) {
+        var fnParamTypes: [fnParams.len]type = undefined;
+        inline for (fnParams, 0..) |p, i| {
+            fnParamTypes[i] = p.type.?;
+        }
+        const msg = std.fmt.comptimePrint("Option {s} does not implement set(){any}, found set({any}){any}", .{ f.name, expectedReturnType, fnParamTypes, fnSpec.return_type });
+        @compileError(msg);
+    }
 }
 
 fn searchParsable(comptime Spec: type, comptime f: std.builtin.Type.StructField, args: []const String, spec: *Spec) ParseError!void {
@@ -96,4 +139,35 @@ test "argParse flag" {
     };
     const parsed = try argParse(args, TestArgs);
     try std.testing.expectEqualDeep(TestArgs{ .verbose = .{ .value = true } }, parsed);
+}
+
+test "argParse missing option" {
+    const TestArgs = struct {
+        a: StringArg,
+        b: StringArg,
+    };
+
+    var args = &[_]String{
+        "--a=AAA",
+    };
+    var parsed = argParse(args, TestArgs);
+    try std.testing.expectError(ParseError.MissingArgument, parsed);
+
+    args = &[_]String{
+        "--b=BBB",
+    };
+    parsed = argParse(args, TestArgs);
+    try std.testing.expectError(ParseError.MissingArgument, parsed);
+}
+
+test "argParse invalid option value" {
+    const TestArgs = struct {
+        b: BoolArg,
+    };
+
+    var args = &[_]String{
+        "--b=not_a_boolean",
+    };
+    var parsed = argParse(args, TestArgs);
+    try std.testing.expectError(ParseError.InvalidValue, parsed);
 }
